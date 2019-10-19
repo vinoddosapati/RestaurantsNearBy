2b5ddf1e81dc1c76b76f5dbc4e5cb40b043936fb
Vinod <vdosapati@albany.edu>
2019-10-19 03:45:40 -0400

    Restaurant api express and angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DetailsComponent},
+  {path: 'profile', component: ProfilePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
